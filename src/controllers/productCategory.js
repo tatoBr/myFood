9aa6629a76a8b6f8f43dac88bcd54bd298ce3f8d
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-04-29 14:01:03 -0300

    update productCategory controller to get category by id

--- src/controllers/productCategory.js
+++ src/controllers/productCategory.js
@@ -1,29 +1,32 @@
 module.exports = class ProductCategoryController{
     getProductCategory( req, res, next ){
+        let message = 'GET Product Category'
+        if( req.params.id )  message+= ' by Id.'
+
         return res.status( 200 ).json({
-            message: 'GET Product Category',
+            message,
             content: null
         });
-    }
+    };
 
     postProductCategory( req, res, next){
         return res.status( 201 ).json({
             message: 'POST Product Category',
             content: null
         });
-    }
+    };
 
     patchProductCategory( req, res, next ){
         return res.status( 202 ).json({
             message: 'PATCH Product Category',
             content: null
         });
-    }
+    };
 
     deleteProductCategory( req, res, next ){
         return res.status( 200 ).json({
             message: 'DELETE Product Category',
             content: null
         });
-    }
+    };
 }
