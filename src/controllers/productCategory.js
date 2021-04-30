bf3e3e70e4764b6d1064c3ca32948e04e209c384
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-04-30 16:06:28 -0300

    Update product category controller to use Services

--- src/controllers/productCategory.js
+++ src/controllers/productCategory.js
@@ -1,7 +1,67 @@
-const Controller = require( './controller' );
+const services = require( '../services/productCategory' )
+const { documentExists } = require( '../utils/databaseHelpers' )
 
-module.exports = class ProductCategoryController extends Controller{
-    constructor(){
-        super('Product Category')
-    }
+module.exports = class ProductCategoryController{
+    post = async ( req, res, next )=>{
+        try {
+            const serviceResponse = await services.create( req.body )
+            const { message, content, status } = serviceResponse;
+
+            return res.status( status ).json({ message, content });
+        } catch ( error ) {
+            next( error );
+        }
+    };
+
+    get = async( req, res, next )=>{
+        try {
+            const serviceResponse = await services.readAll();
+            const { message, content, status } = serviceResponse;
+
+            return res.status( status ).json({ message, content });
+
+        } catch ( error ) {
+            next( error );
+        }
+    };
+
+    getById = async ( req, res, next )=>{
+        try {          
+            const { id } = req.params;
+
+            const serviceResponse = await services.readById( id );
+            const { message, content, status } = serviceResponse;
+
+            return res.status( status ).json({ message, content });      
+        } catch (error) {
+            next( error );
+        }
+    };
+    
+    patch = async ( req, res, next )=>{
+        try {          
+            const { id } = req.params;
+            const data  = req.body;
+
+            const serviceResponse = await services.update( id, data );
+            const { message, content, status } = serviceResponse;
+
+            return res.status( status ).json({ message, content });      
+        } catch (error) {
+            next( error );
+        }
+    };
+
+    delete = async ( req, res, next )=>{
+        try {          
+            const { id } = req.params;            
+
+            const serviceResponse = await services.delete( id );
+            const { message, content, status } = serviceResponse;
+
+            return res.status( status ).json({ message, content });      
+        } catch (error) {
+            next( error );
+        };
+    };
 }
