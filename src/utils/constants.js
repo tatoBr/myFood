508ed17123123d346642cb86729602681625c0d0
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-04-30 16:03:25 -0300

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+module.exports = {
+    httpStatusCodes:{
+        OK: 200,
+        CREATED: 201,
+        ACCEPTED: 202,
+        NO_CONTENT: 204,
+        BAD_REQUEST: 400,
+        NOT_FOUND: 404,
+        CONFLICT: 409
+    }
+}
