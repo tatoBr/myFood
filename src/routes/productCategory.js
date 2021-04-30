6793d63115b4e5c26870f69ca2db4e631b98fe50
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-04-30 16:04:54 -0300

    Update productCategory route to use getById controller

--- src/routes/productCategory.js
+++ src/routes/productCategory.js
@@ -6,7 +6,7 @@ const controller = new Controller();
 
 router.post('/', controller.post );
 router.get('/', controller.get );
-router.get('/:id', controller.get );
+router.get('/:id', controller.getById );
 router.patch('/:id', controller.patch );
 router.delete('/:id', controller.delete );
 
