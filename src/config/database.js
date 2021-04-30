fb08e7ab69bc7646db953e5459b499790a149948
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-04-30 13:54:01 -0300

    Create database config file

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+if( process.env.NODE_ENV !== "production" )
+    require("dotenv").config();
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+
+module.exports = {
+    uri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.o77px.${dbHost}/${dbName}?retryWrites=true&w=majority`,
+    options:{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex:true              
+    }
+};
